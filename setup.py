b5fda9edaa1bb70135595b87aa9d3e45fdea9e82
Ludwig Leonard <lleonart1984@gmail.com>
2025-02-25 21:52:17 +0100

    refactoring

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cloudy",
+    version="1.0.0",                       # Semantic versioning
+    author="Ludwic Leonard",
+    author_email="lleonart1984@gmail.com",
+    description="Diffusion Model for clouds.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/rendervous/cloudy_project",
+    packages=find_packages(where="src"),  # Look for packages in "src"
+    package_dir={"": "src"},  # Map the root package directory to "src"
+    # packages=find_packages(),             # Automatically find packages in your project
+    install_requires=[
+        "rendervous @ git+https://github.com/rendervous/rendervous_project.git"
+    ],
+    include_package_data=True,
+    extras_requires={
+        "gui": [
+            "pywin32; platform_system == 'Windows'",
+            "pygobject; platform_system == 'Linux'",
+            "pyobjc; platform_system == 'Darwin'",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',              # Specify supported Python versions
+)
